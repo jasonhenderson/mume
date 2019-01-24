4c0c3ec1cd3c7c51cc7ef4f39d3f66281c4e988f
Jason Henderson <jason@mbay.io>
2019-01-24 15:26:32 -0800

    repair attrs

--- src/custom-markdown-it-features/attrs.ts
+++ src/custom-markdown-it-features/attrs.ts
@@ -1,7 +1,7 @@
 // tslint:disable-next-line no-implicit-dependencies
 import { MarkdownIt } from "markdown-it";
 import { MarkdownEngineConfig } from "../markdown-engine-config";
-import { MarkdownItAttrs } from "markdown-it-attrs";
+import MarkdownItAttrs from  "markdown-it-attrs";
 
 export default (md: MarkdownIt, config: MarkdownEngineConfig) => {
   md.use(MarkdownItAttrs);
