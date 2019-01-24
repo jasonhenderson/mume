eeb07a6337931c2af2cd0be122b5010c6a005401
Jason Henderson <jason@mbay.io>
2019-01-24 14:50:35 -0800

    added support for custom markdown attributes

---
+++ src/custom-markdown-it-features/attrs.ts
@@ -0,0 +1,8 @@
+// tslint:disable-next-line no-implicit-dependencies
+import { MarkdownIt } from "markdown-it";
+import { MarkdownEngineConfig } from "../markdown-engine-config";
+import { MarkdownItAttrs } from "markdown-it-attrs";
+
+export default (md: MarkdownIt, config: MarkdownEngineConfig) => {
+  md.use(MarkdownItAttrs);
+};
